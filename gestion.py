28a2477aa4e9807a922af485329e2a99790047f0
Rodrigo Cristaldo <r.czaya.nyc@gmail.com>
2024-10-24 15:23:10 -0300

    Funcion Agregar Producto

--- gestion.py
+++ gestion.py
@@ -14,7 +14,6 @@ class Producto(db.Model):
     descripcion = db.Column(db.String(255))
     categoria = db.Column(db.String(50))
 
-
 @app.route('/productos', methods=['POST'])
 def agregar_producto():
     data = request.get_json()
@@ -29,34 +28,6 @@ def agregar_producto():
     db.session.commit()
     
     return jsonify({'id': nuevo_producto.id, 'nombre': nuevo_producto.nombre}), 201
-
-
-@app.route('/productos', methods=['GET'])
-def listar_productos():
-    productos = Producto.query.all()
-    
-    return jsonify([{'id': p.id, 'nombre': p.nombre, 'precio': p.precio, 'descripcion': p.descripcion, 'categoria': p.categoria} for p in productos]), 200
-
-
-@app.route('/productos/<int:id>', methods=['PUT'])
-def actualizar_producto(id):
-    data = request.get_json()
-    producto = Producto.query.get_or_404(id)
-    producto.nombre = data['nombre']
-    producto.precio = data['precio']
-    producto.descripcion = data.get('descripcion', producto.descripcion)
-    producto.categoria = data.get('categoria', producto.categoria)
-    db.session.commit()
-    
-    return jsonify({'id': producto.id, 'nombre': producto.nombre}), 200
-
-
-@app.route('/productos/<int:id>', methods=['DELETE'])
-def eliminar_producto(id):
-    producto = Producto.query.get_or_404(id)
-    db.session.delete(producto)
-    db.session.commit()
-    return jsonify({'message': f'Producto_id: {id}, eliminado con éxito'}), 200
 
 
 
